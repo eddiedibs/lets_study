03f5d80d6cd907804f9b74f4cf1f7b4adc19aca9
mfs-eduardocarmona <liledwa@gmail.com>
2024-06-19 13:00:55 -0400

    Added cubits to state management across pages

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,13 @@ import 'package:lets_study_flutter/design/activity_card.dart';
 import 'package:lets_study_flutter/design/custom_app_bar.dart';
 import 'package:lets_study_flutter/design/custom_bottom_app_bar.dart';
 import 'package:lets_study_flutter/design/components/activity_card_progress_component.dart';
-import 'package:lets_study_flutter/design/components/activity_card_courses_component.dart';
 import 'package:lets_study_flutter/design/components/activity_card_education_component.dart';
 
+import 'package:lets_study_flutter/providers/activity_cubits.dart';
+
 void main() {
-  runApp(ModularApp(module: AppModule(), child:MyActivityApp()));
-  // runApp(MyActivityApp());  
-  
-  }
+  runApp(ModularApp(module: AppModule(), child: MyActivityApp()));
+}
 
 class MyActivityApp extends StatelessWidget {
   @override
@@ -33,114 +32,87 @@ class MyActivityApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(titleText: "My Activity", isHomePage: true),
-      body: Container(
-        margin: const EdgeInsets.symmetric(vertical: 40),
-        height: double.infinity, // Ensure the container takes full height
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Expanded(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                      SizedBox(
-                          height: 200,
-                          width: 180,
-                          child: Center(
-                            child: InkWell(
-                              onTap: () {
-                                Modular.to.pushNamed('/progress-page');
-                              },
-                              child: ActivityCard(
-                                  title: 'Progreso',
-                                  titleSize: 24,
-                                  color: const Color.fromARGB(255, 38, 46, 92),
-                                  widgetComponent: [ProgressComponent(
-                                        progressComponentType: ProgressComponentType.circle,
-                                        progress: 0.7,
-                                        addProgressPercentage: true,
-                                        progressColor: const Color.fromARGB(255, 248, 34, 156),
-                                        progressBgColor: const Color.fromARGB(255, 60, 69, 110),
-                                      )],
-                                  iconColor: const Color.fromARGB(255, 248, 34, 156),
-                                  icon: Icons.local_fire_department,
+    return BlocProvider(
+      create: (context) => ActivityCubit()..loadActivities(),
+      child: Scaffold(
+        appBar: CustomAppBar(titleText: "My Activity", isHomePage: true),
+        body: Container(
+          margin: const EdgeInsets.symmetric(vertical: 40),
+          height: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: BlocBuilder<ActivityCubit, ActivityState>(
+              builder: (context, state) {
+                if (state is ActivityLoaded) {
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: state.activities.take(2).map((activity) {
+                          return SizedBox(
+                            height: 200,
+                            width: 180,
+                            child: Center(
+                              child: InkWell(
+                                onTap: () {
+                                  Modular.to.pushNamed('/progress-page');
+                                },
+                                child: ActivityCard(
+                                  title: activity.activityCard.title,
+                                  titleSize: activity.activityCard.titleSize,
+                                  color: activity.activityCard.color,
+                                  widgetComponent: [
+                                    ...activity.activityCard.widgetComponent
+                                  ],
+                                  iconColor: activity.activityCard.iconColor,
+                                  icon: activity.activityCard.icon,
                                 ),
+                              ),
                             ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 200,
-                          width: 180,
-                          child: Center(
-                            child: InkWell(
-                              onTap: () {
-                                Modular.to.pushNamed('/progress-page');
-                              },
-                              child: ActivityCard(
-                                  title: 'Progreso',
-                                  titleSize: 24,
-                                  color: const Color.fromARGB(255, 38, 46, 92),
-                                  widgetComponent: [ProgressComponent(
-                                        progressComponentType: ProgressComponentType.circle,
-                                        progress: 0.7,
-                                        addProgressPercentage: true,
-                                        progressColor: const Color.fromARGB(255, 248, 34, 156),
-                                        progressBgColor: const Color.fromARGB(255, 60, 69, 110),
-                                      )],
-                                  iconColor: const Color.fromARGB(255, 248, 34, 156),
-                                  icon: Icons.local_fire_department,
+                          );
+                        }).toList(),
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: state.activities.skip(2).take(1).map((activity) {
+                          return SizedBox(
+                            height: 320,
+                            width: 180,
+                            child: Center(
+                              child: InkWell(
+                                onTap: () {
+                                  Modular.to.pushNamed('/progress-page');
+                                },
+                                child: ActivityCard(
+                                  title: activity.activityCard.title,
+                                  titleSize: activity.activityCard.titleSize,
+                                  color: activity.activityCard.color,
+                                  widgetComponent: [
+                                    ...activity.activityCard.widgetComponent
+                                  ],
+                                  iconColor: activity.activityCard.iconColor,
+                                  icon: activity.activityCard.icon,
                                 ),
+                              ),
                             ),
-                          ),
-                        ),
-                  ],
-                ),
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                      SizedBox(
-                          height: 320,
-                          width: 180,
-                          child: Center(
-                            child: InkWell(
-                              onTap: () {
-                                Modular.to.pushNamed('/progress-page');
-                              },
-                              child: ActivityCard(
-                                  title: 'Perfil',
-                                  titleSize: 15,
-                                  color: Color.fromARGB(255, 255, 255, 255),
-                                  widgetComponent: [EducationComponent(
-                                    cardTitle: "John Doe",
-                                    cardTitleSize: 20,
-                                    cardDescription: "Estudiante de ingenieria de software",
-                                    imgUrl: "https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg",
-                                  )],
-                                  iconColor: const Color.fromARGB(255, 52, 164, 255),
-                                  icon: Icons.person,
-                                ),
-                            ),
-                          ),
-                        ),
-                  ],
-                )
-                
-                
-              ],
+                          );
+                        }).toList(),
+                      ),
+                    ],
+                  );
+                } else {
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
             ),
           ),
         ),
+        bottomNavigationBar: CustomBottomAppBar(),
       ),
-      bottomNavigationBar: CustomBottomAppBar(),
     );
   }
 }
-
-
---
+++ lib/providers/activity_cubits.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:lets_study_flutter/design/components/activity_card_progress_component.dart';
+import 'package:lets_study_flutter/design/components/activity_card_education_component.dart';
+import 'package:lets_study_flutter/design/activity_card.dart';
+
+part 'activity_state.dart';
+
+class ActivityCubit extends Cubit<ActivityState> {
+  ActivityCubit() : super(ActivityInitial());
+
+  void loadActivities() {
+    // Simulate loading activities, you can fetch data from API or database here
+    emit(ActivityLoaded([
+      Activity(
+        activityCard:ActivityCard(
+          title: 'Progreso',
+          titleSize: 24,
+          color: const Color.fromARGB(255, 38, 46, 92),
+          widgetComponent: [
+            ProgressComponent(
+              progressComponentType: ProgressComponentType.circle,
+              progress: 0.7,
+              addProgressPercentage: true,
+              progressColor: const Color.fromARGB(255, 248, 34, 156),
+              progressBgColor: const Color.fromARGB(255, 60, 69, 110),
+            )
+          ],
+          iconColor: const Color.fromARGB(255, 248, 34, 156),
+          icon: Icons.local_fire_department,
+        )
+        
+      ),
+      Activity(
+        activityCard:ActivityCard(
+          title: 'Quizzes',
+          titleSize: 24,
+          color: const Color.fromARGB(255, 38, 46, 92),
+          widgetComponent: [
+            ProgressComponent(
+              progressComponentType: ProgressComponentType.circle,
+              progress: 0.7,
+              addProgressPercentage: true,
+              progressColor: const Color.fromARGB(255, 248, 34, 156),
+              progressBgColor: const Color.fromARGB(255, 60, 69, 110),
+            )
+          ],
+          iconColor: const Color.fromARGB(255, 248, 34, 156),
+          icon: Icons.local_fire_department,
+        )
+      ),
+      Activity(
+        activityCard: ActivityCard(
+          title: 'Perfil',
+          titleSize: 15,
+          color: Color.fromARGB(255, 255, 255, 255),
+          widgetComponent: [
+            EducationComponent(
+              imgUrl: "https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg",
+              cardTitle: "John Doe",
+              cardTitleSize: 20,
+              cardDescription: "Estudiante de ingeniería de software",
+            )
+          ],
+          iconColor: const Color.fromARGB(255, 52, 164, 255),
+          icon: Icons.person,
+        )
+      ),
+    ]));
+  }
+}
